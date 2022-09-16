d9a2d8cb56ffbf34457ae29ed3f46016965358af
tudormihail5 <113465589+tudormihail5@users.noreply.github.com>
2022-09-16 21:14:04 +0300

    Update Types.hs

--- Types.hs
+++ Types.hs
@@ -1,14 +1,3 @@
--- setting the "warn-incomplete-patterns" flag asks GHC to warn you
--- about possible missing cases in pattern-matching definitions
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-
--- see https://wiki.haskell.org/Safe_Haskell
-{-# LANGUAGE Safe #-}
-
----------------------------------------------------------------------------------
--------------------------- DO **NOT** MODIFY THIS FILE --------------------------
----------------------------------------------------------------------------------
-
 module Types where
 
 {- Exercise 1 -}
